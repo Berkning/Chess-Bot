8f6061a715da3bcfc0b9db3710c36f8a9915a19c
Berkning <berkning@outlook.com>
2025-09-12 13:18:25 +0200

    Expanded perft suite

--- Assets/Scripts/Perft.cs
+++ Assets/Scripts/Perft.cs
@@ -117,18 +117,44 @@ public static class Perft
         new SuiteComponent("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 6, 11030083),
         new SuiteComponent("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 5, 15833292),
         new SuiteComponent("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 5,  89941194),
-        new SuiteComponent("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 5, 164075551)
+        new SuiteComponent("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 5, 164075551),
+        new SuiteComponent("r3k2r/8/8/8/8/8/8/R3K2R b KQkq - 0 1", 6, 179862938),
+        new SuiteComponent("8/8/1B6/7b/7k/8/2B1b3/7K w - - 0 1", 6, 28861171),
+        new SuiteComponent("R6r/8/8/2K5/5k2/8/8/r6R w - - 0 1", 5, 20506480),
+        new SuiteComponent("6KQ/8/8/8/8/8/8/7k b - - 0 1", 7, 1750864),
+        new SuiteComponent("8/2k1p3/3pP3/3P2K1/8/8/8/8 w - - 0 1", 9, 7618365),
+        new SuiteComponent("K7/p7/k7/8/8/8/8/8 b - - 0 1", 10, 10949673),
+        new SuiteComponent("8/8/3k4/3p4/8/3P4/3K4/8 b - - 0 1", 8, 7572916),
+        new SuiteComponent("k7/8/8/3p4/4p3/8/8/7K w - - 0 1", 9, 5871381),
+        new SuiteComponent("7k/8/8/1p6/P7/8/8/7K w - - 0 1", 9, 16217575),
+        new SuiteComponent("3k4/3pp3/8/8/8/8/3PP3/3K4 w - - 0 1", 9, 121183847),
+        new SuiteComponent("n1n5/1Pk5/8/8/8/8/5Kp1/5N1N w - - 0 1", 7, 690692460),
+        new SuiteComponent("8/PPPk4/8/8/8/8/4Kppp/8 b - - 0 1", 7, 614154982),
     };
 
     public static void RunFullSuite() //Wont keep current board position
     {
-        foreach (SuiteComponent component in testSuite)
+        MoveGenerator.PromotionMode prevPromotionMode = MoveGenerator.promotionMode; //Save what the promotionmode was set to
+        MoveGenerator.promotionMode = MoveGenerator.PromotionMode.All; //Set the promotion mode to all to ensure we get all possible moves
+
+        bool passedAll = true;
+
+        for (int i = 0; i < testSuite.Length; i++)
         {
+            SuiteComponent component = testSuite[i];
+
             FenUtility.LoadPositionFromFen(component.fen);
             long result = RunSpecifiedDepth(component.depth);
             bool passed = result == component.correctResult;
-            Debug.Log("Fen: " + component.fen + " Ply: " + component.depth + " Nodes: " + result + " Passed: " + passed + (passed ? "" : " Expected: " + component.correctResult));
+
+            if (!passed) passedAll = false;
+            //Debug.Log("Fen: " + component.fen + " Ply: " + component.depth + " Nodes: " + result + " Passed: " + passed + (passed ? "" : " Expected: " + component.correctResult));
+            Debug.Log("Test " + (i + 1) + " - Ply: " + component.depth + " Nodes: " + result + " Passed: " + passed + (passed ? "" : " Expected: " + component.correctResult) + "   Fen: " + component.fen);
         }
+
+        Debug.Log(passedAll ? "Suite Passed" : "Suite Failed");
+
+        MoveGenerator.promotionMode = prevPromotionMode;
     }
 
     private struct SuiteComponent
