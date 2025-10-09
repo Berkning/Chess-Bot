d2f7706de31e260c79f9e43aca0ca13ee3f38a5d
Berkning <berkning@outlook.com>
2025-10-09 17:37:23 +0200

    Moved to Studio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+public static class Program
+{
+    public static UnityPipe pipe = new UnityPipe();
+
+    public static void Main(string[] args)
+    {
+        EngineUCI engine = new EngineUCI();
+        //pipe.InitializePipe();
+
+        string message = String.Empty;
+
+        while (message != "quit")
+        {
+            message = Console.ReadLine();
+            engine.RecieveCommand(message);
+        }
+
+        //pipe.shouldShutDown = true;
+    }
+}
