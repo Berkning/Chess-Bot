f09efa03f002480ea34359621714d7381ff75444
Berkning <berkning@outlook.com>
2026-07-31 13:17:10 +0200

    Revert "temp full-window NMP search"
    This reverts commit d6a86dda521b1b6dfd122bbafb93785d1f57f332.

--- Game Essential/Board.cs
+++ Game Essential/Board.cs
@@ -503,7 +503,7 @@ public class Board //TODOnt prob: Try maybe changing to struct?
         if (prevEpFile != -1)
         {
             currentZobrist ^= Zobrist.epArray[prevEpFile]; //Remove old ep file
-            currentGameState ^= prevGameState & epFileMask; //TODO: can be simplified to currentGameState &= ~epFileMask
+            currentGameState ^= prevGameState & epFileMask;
         }
 
         gameStateHistory.Push(currentGameState);
