39a436f112e1decf557512e0a150c3905c48c070
Berkning <berkning@outlook.com>
2026-09-11 11:23:58 +0200

    Revert "only run full-window on pv nodes"
    This reverts commit 232a92a9326dcb4c24d284591810186f6720fb1e.

--- Search/Search.cs
+++ Search/Search.cs
@@ -341,7 +341,6 @@ public class Search
             //Late Move Reduction
             if (i > 4 && extensions == 0 && depth > 3)  //Assuming move ordering isn't completely wrong
             {
-                //TODO: try moving the pvs stuff in here as well to make it cleaner/maybe fix pvs being worse than plain A/B
                 evaluation = -AlphaBeta(depth - 2, plyFromRoot + 1, -alpha - 1, -alpha, numExtensions, false);
 
                 //If evals better than anything else so far we'll search to full depth
@@ -350,7 +349,7 @@ public class Search
 
             if (searchFullDepth)
             {
-                if (isPV && i == 0) evaluation = -AlphaBeta(depth - 1 + extensions, plyFromRoot + 1, -beta, -alpha, numExtensions + extensions, true);//, test);
+                if (i == 0) evaluation = -AlphaBeta(depth - 1 + extensions, plyFromRoot + 1, -beta, -alpha, numExtensions + extensions, isPV);//, test);
                 else
                 {
                     evaluation = -AlphaBeta(depth - 1 + extensions, plyFromRoot + 1, -alpha - 1, -alpha, numExtensions + extensions, false);
