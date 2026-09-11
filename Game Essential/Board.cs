e95c2cf217143e9fffebc5a050b49303ecfbffd3
Berkning <berkning@outlook.com>
2026-09-11 15:33:01 +0200

    Revert "dont increment nodecount before timer check"
    This reverts commit 99aab3b42ca8da1465dc6dd01b1ffd57fe124193.

--- Game Essential/Board.cs
+++ Game Essential/Board.cs
@@ -294,7 +294,7 @@ public class Board //TODOnt prob: Try maybe changing to struct?
         }
         else if (movedPieceType == Piece.Rook)
         {
-            //TODO: Can remove this entire else if with a simple switch at the start of this function - along with other place we are checking this
+            //TODO: Could maybe check if castling is even allowed to avoid unnecessary stuff
             if (enemyColor == Piece.White) //Means white played the move
             {
                 if (move.startSquare == BoardHelper.h1) //White shortcastle rook
