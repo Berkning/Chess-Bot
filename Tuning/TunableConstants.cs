c8af4b16f6fa86e264ac4bb5c7c352373e2a66a9
Berkning <berkning@outlook.com>
2026-09-15 20:00:20 +0200

    added tuned constants
    really doubt this turned out quite right with the reduction formula

--- Tuning/TunableConstants.cs
+++ Tuning/TunableConstants.cs
@@ -54,10 +54,10 @@ public static class TunableConstants
     public static TunableConstant aspWindowIncrement5 = Tuning.AddConstant(new TunableConstant("AspWindowIncrement5", Category.Search, 800, 0, 3000));
     public static TunableConstant aspWindowIncrement6 = Tuning.AddConstant(new TunableConstant("AspWindowIncrement6", Category.Search, 1600, 0, 9000));
 
-    public static TunableConstant nMPPhaseCutoff = Tuning.AddConstant(new TunableConstant("NMPPhaseCutoff", Category.Search, 24, 0, 25));
-    public static TunableConstant nMPReduction1 = Tuning.AddConstant(new TunableConstant("NMPReduction1", Category.Search, 0, 0, 256));
-    public static TunableConstant nMPReduction2 = Tuning.AddConstant(new TunableConstant("NMPReduction2", Category.Search, 16, 1, 256));
-    public static TunableConstant nMPReduction3 = Tuning.AddConstant(new TunableConstant("NMPReduction3", Category.Search, 48, 0, 256));
+    public static TunableConstant nMPPhaseCutoff = Tuning.AddConstant(new TunableConstant("NMPPhaseCutoff", Category.Search, 23, 0, 25));
+    public static TunableConstant nMPReduction1 = Tuning.AddConstant(new TunableConstant("NMPReduction1", Category.Search, 16, 0, 256));
+    public static TunableConstant nMPReduction2 = Tuning.AddConstant(new TunableConstant("NMPReduction2", Category.Search, 48, 1, 256));
+    public static TunableConstant nMPReduction3 = Tuning.AddConstant(new TunableConstant("NMPReduction3", Category.Search, 61, 0, 256));
 
     public static int AspInstabilityMargin {get {return aspInstabilityMargin.currentValue;}}
     public static int AspWindowIncrement0 {get {return aspWindowIncrement0.currentValue;}}
@@ -83,10 +83,10 @@ public static class TunableConstants
     public const int AspWindowIncrement5 = 800;
     public const int AspWindowIncrement6 = 1600;
 
-    public const int NMPPhaseCutoff = 24;
-    public const int NMPReduction1 = 0;
-    public const int NMPReduction2 = 16;
-    public const int NMPReduction3 = 48;
+    public const int NMPPhaseCutoff = 23;
+    public const int NMPReduction1 = 16;
+    public const int NMPReduction2 = 48;
+    public const int NMPReduction3 = 61;
 #endif
 
     //------------------------------------------------------------
